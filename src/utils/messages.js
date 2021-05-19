762f1190e7a62b26951000a02ded37d6ec837bbd
clarafonseca <clarafonseca@users.noreply.github.com>
2021-05-19 00:10:32 -0300

    add utils

---
+++ src/utils/messages.js
@@ -0,0 +1,9 @@
+module.exports.messages = {
+  notFound: (resource) => `${resource}-not-found`,
+  alreadyExists: (param) => `${param}-already-registered`,
+  invalidFields: 'invalid-fields',
+  invalidPassword: 'invalid-password',
+  emailUnavailable: 'email-unavailable',
+  accessUnauthorized: 'access-unauthorized',
+  internalError: 'internal-server-error'
+}
