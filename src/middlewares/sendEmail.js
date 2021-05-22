ca13c82b186b99d516875deb41be0c7a9ee02a3e
clarafonseca <clarafonseca@users.noreply.github.com>
2021-05-22 12:53:54 -0300

    ADAPTO-63 getting rid of some unnecessary details

--- src/middlewares/sendEmail.js
+++ src/middlewares/sendEmail.js
@@ -1,7 +1,7 @@
 const nodemailer = require('nodemailer')
 const SMTP_CONFIG = require('../config/smtp')
 
-module.exports = async (req, res, next) => {
+module.exports = async (req, res) => {
   try {
     const transporter = nodemailer.createTransport({
       host: SMTP_CONFIG.host,
@@ -15,28 +15,20 @@ module.exports = async (req, res, next) => {
         rejectUnauthorized: false
       }
     })
-    const code = Math.random().toString(36).substring(7)
-    const userEmail = req.user.email
-    console.log(userEmail)
 
     await transporter.sendMail({
-      text: 'Código para recuperação de senha',
-      subject: 'Código para recuperação de senha',
+      text: 'Variavel Texto',
+      subject: 'Variavel assunto',
       from: `Equipe Adapto <${SMTP_CONFIG.user}`,
-      to: [SMTP_CONFIG.user, userEmail],
+      to: [SMTP_CONFIG.user],
       html: `
         <html>
         <body>
-          <strong>${code}</strong>
+          <strong>Test</strong>
         </body>
       </html> 
         `
     })
-    req.recover = {
-      code: code
-    }
-
-    return next()
   } catch (error) {
     console.error(error)
     return res.status(error.status).json(error.message)
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,11 @@
 const router = require('express').Router()
 const { userController } = require('../controllers')
-const { isAuthenticated, sendEmail } = require('../middlewares')
+const { isAuthenticated } = require('../middlewares')
 
 router.post('/', userController.createTest)
 router.get('/', isAuthenticated, userController.list)
 router.get('/:id', userController.get)
 router.put('/', isAuthenticated, userController.update)
-router.patch('/', isAuthenticated, sendEmail)
 router.delete('/', isAuthenticated, userController.delete)
 
 module.exports.user = router
