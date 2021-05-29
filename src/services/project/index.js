56b21a0ac1e03850ac7cb528e80b11f85578ac7e
clarafonseca <62162700+clarafonseca@users.noreply.github.com>
2021-05-29 04:01:02 -0300

    list  structure

--- src/services/project/index.js
+++ src/services/project/index.js
@@ -2,10 +2,12 @@ const { create } = require('./create.service')
 const { get } = require('./get.service')
 const { update } = require('./update.service')
 const { deleteOne } = require('./delete.service')
+const { list } = require('./list.service')
 
 module.exports = {
   create,
   get,
   update,
-  deleteOne
+  deleteOne,
+  list
 }
