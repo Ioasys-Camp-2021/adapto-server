26510688b150991b69d785817ba058e6e9e2ccc2
clarafonseca <62162700+clarafonseca@users.noreply.github.com>
2021-06-01 21:18:26 -0300

    test sequelize heroku

--- src/config/database/sequelize.js
+++ src/config/database/sequelize.js
@@ -1,14 +1,23 @@
 require('dotenv/config')
 
 module.exports = {
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  dialect: 'postgres',
-  define: {
-    timestamps: true,
-    underscored: true
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+    define: {
+      timestamps: true,
+      underscored: true
+    }
+  },
+  production: {
+    use_env_variable: process.env.DATABASE_URL,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: true
+    }
   }
 }
